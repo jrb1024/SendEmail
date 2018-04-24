9464030ca407fc7ff66c98e7d26b97177580e347
jrb1024 <38698175+jrb1024@users.noreply.github.com>
2018-04-24 12:00:13 -0400

    Adding main batch file

---
+++ SendEmail.bat
@@ -0,0 +1,34 @@
+@ECHO ON  
+
+::   ################################################################################
+::   ##
+::   ##                     SendEmail.bat  VERSION 1.0  	  
+::   ##                  ------------------------------------
+::   ##  
+::   ##  This script copies an email file to the SMTP pickup folder based on email status vairable
+::   ##  
+::   ##  Script location: C:\JDA\scripts
+::   ##
+::   ##  *Modified:
+::   ##  ----------
+::   ##  * JB 04/03/2018 -- Initial creation 
+::   ################################################################################
+
+::Set EMAILSTATUS=Success
+goto %EMAILSTATUS%
+
+:Start
+xcopy /y C:\JDA\scripts\RestartJDAEmailStart.txt C:\inetpub\mailroot\Pickup\RestartJDAEmailStart.txt*
+goto FINISH
+
+:Success
+xcopy /y C:\JDA\scripts\RestartJDAEmailSuccess.txt C:\inetpub\mailroot\Pickup\RestartJDAEmailSuccess.txt*
+goto FINISH
+
+:Error
+xcopy /y C:\JDA\scripts\RestartJDAEmailError.txt C:\inetpub\mailroot\Pickup\RestartJDAEmailError.txt*
+goto FINISH
+
+
+:FINISH
+EXIT /B 
